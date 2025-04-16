c06e90ce4800766350ef0543191c717c5b6e17b4
Thumdeo <thumdeowarhade@gmail.com>
2025-04-16 17:20:36 +0530

    The data updated Dao progress

--- Blogger/src/jdbc/Dao.java
+++ Blogger/src/jdbc/Dao.java
@@ -5,24 +5,5 @@ import java.sql.PreparedStatement;
 
 import entity.*;
 public class Dao extends AutoCloseable {
-	private Connection con;
-	
-	
-	@Override
-	public int save(User s) throws Exception {
-		int count; 
-		String sql = "INSERT INTO students VALUES(?,?,?,?,?,?,?)"; 
-		try(PreparedStatement stmt = con.prepareStatement(sql)){
-			stmt.setInt(1, s.getId());
-			stmt.setString(2, s.getFull_Name());
-			stmt.setString(3, s.getEmail());
-			stmt.setString(4, s.getPassword());
-			stmt.setInt(5, s.getPhone_no());
-			stmt.setInt(6, s.getPhone_no());
-			stmt.setDate(7, (Date) s.getCreated_time());
-			
-			count = stmt.executeUpdate(); 
-		}
-		return count;
-	}
+	int save(User u) throws Exception ; 
 }
--- Blogger/src/jdbc/DaoImpl.java
+++ Blogger/src/jdbc/DaoImpl.java
@@ -1,5 +1,39 @@
 package jdbc;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import entity.*;
 public class DaoImpl implements AutoCloseable ,Dao{
-  
+	private Connection con;
+	public  Dao() throws Exception{
+		con =util.getConnection();
+	}
+	@Override
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	@Override
+	public list save(User u) throws Exception {
+		int count; 
+		String sql = "INSERT INTO User VALUES(?,?,?,?,?,?,?)"; 
+		try(PreparedStatement stmt = con.prepareStatement(sql)){
+			stmt.setInt(1, u.getId());
+			stmt.setString(2, u.getFull_Name());
+			stmt.setString(3, u.getEmail());
+			stmt.setString(4, u.getPassword());
+			stmt.setInt(5, u.getPhone_no());
+			stmt.setDate(6, (Date) u.getCreated_time());
+			stmt.setString(7, u.getAddress());
+			
+			count = stmt.executeUpdate(); 
+		}
+		return count;
+	}
 }
